5804164d66e8cff5ca6c3974b33f52e443e75aeb
TwoKe <2956917032@qq.com>
2022-12-03 06:34:56 +0800

    feat: 部署gitee

--- src/.vuepress/.temp/pages/floatinglife/index.html.vue
+++ src/.vuepress/.temp/pages/floatinglife/index.html.vue
@@ -1,4 +1,7 @@
 <template><div><h1 id="浮生杂记" tabindex="-1"><a class="header-anchor" href="#浮生杂记" aria-hidden="true">#</a> 浮生杂记</h1>
+<blockquote>
+<p>github 部署</p>
+</blockquote>
 </div></template>
 
 

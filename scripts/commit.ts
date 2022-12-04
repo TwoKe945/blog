863b9ddfc7bf48d9ec5c54073bd73c42509e2e8f
TwoKe <2956917032@qq.com>
2022-12-05 02:16:21 +0800

    chore: 修改测试

--- scripts/commit.ts
+++ scripts/commit.ts
@@ -2,12 +2,21 @@ import { withTaskName, run } from './utils'
 import { series } from 'gulp'
 import { getLastCommit } from 'git-last-commit'
 
+function getMessage(body: string) {
+  const contents = body.split(":");
+  if (contents.length >= 2) {
+    return contents.slice(1).join("")
+  } else {
+    return contents[0]
+  }
+}
+
 export default series(
   withTaskName('编译博客', () => run('pnpm docs:build')),
   withTaskName('暂存编译博客', () => run('git add .')),
   withTaskName('添加提交记录', () => {
     getLastCommit((err, commit) => {
-      run(`git commit -m "deploy:${commit.body}"`)
+      run(`git commit -m "deploy:${getMessage(commit.subject)}"`)
     })
   }),
   withTaskName('更新远程仓库', () => run(`git push`)),
